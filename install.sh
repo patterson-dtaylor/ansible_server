45d34c9b284a99028a39d63174fce3889a753135
Taylor Patterson <patterson.dtaylor@gmail.com>
2022-04-16 15:32:06 -0400

    Ansible install code added to install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+echo "Updating System..."
+sudo apt update && sudo apt upgrade -y
+
+ech "Installing Ansible..."
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+
+echo "Ansible Version"
+sudo ansible --version
+
 echo "Everything looks good as of now!"
